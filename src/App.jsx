95587fd2070806fed1f46339a237a85830b47b7d
Panashe Matsaudza <pmatsaudza@gmail.com>
2018-08-26 00:55:57 +0200

    uploaded files via upload
    reactjs countdown timer final

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React ,{Component} from 'react';
+import Clock from './Clock';
+import {Form ,FormControl,Button} from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+    deadline: 'December 25,2018',
+    newDeadline:''
+    }
+  }
+
+  changeDeadline(){
+    this.setState({deadline: this.state.newDeadline})
+  }
+  render() {
+    return(
+      <div className ='App'>
+        <div className="App-title">Countdown to {this.state.deadline}</div>
+<Clock
+deadline ={this.state.deadline}
+/>
+      <Form inline>
+      <FormControl className = "Deadline-input" placeholder ='new date'
+      onChange={event => this.setState({newDeadline:event.target.value})}/>
+      <Button Click ={() => this.changeDeadline()}>Submit</Button>
+
+        </Form>
+        </div>
+    )
+  }
+}
+export default App;
